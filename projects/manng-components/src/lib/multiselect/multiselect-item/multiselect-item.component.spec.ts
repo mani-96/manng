f413a3652bd24d847b841e8cf51cb3c9d980bea8
Manish Sharma <msmanishsharma52@gmail.com>
2021-07-01 16:11:33 +0530

    Writing test cases for multiselect

--- projects/manng-components/src/lib/multiselect/multiselect-item/multiselect-item.component.spec.ts
+++ projects/manng-components/src/lib/multiselect/multiselect-item/multiselect-item.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { emitKeypressEvents } from 'readline';
 
 import { MultiselectItemComponent } from './multiselect-item.component';
 
@@ -8,9 +10,9 @@ describe('MultiselectItemComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MultiselectItemComponent ]
+      declarations: [MultiselectItemComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +24,50 @@ describe('MultiselectItemComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should give correct option value when no field value is present', () => {
+    let optionValue = 'Option Value';
+    component.option = optionValue;
+    expect(component.getOptionValue()).toBe('Option Value')
+  })
+
+  it('should give correct result when field is give', () => {
+    let optionValue = {
+      value: 'Option Value'
+    }
+    let field = 'value'
+    component.option = optionValue;
+    component.field = field;
+    expect(component.getOptionValue()).toBe('Option Value')
+  })
+
+  it('should emit event when clicked', () => {
+    component.option = 'Option value'
+    component.isDisabled = false;
+    spyOn(component.onOptionClick, 'emit')
+    let li = fixture.debugElement.query(By.css('li'));
+    li.nativeElement.click();
+    fixture.detectChanges();
+    expect(component.onOptionClick.emit).toHaveBeenCalled();
+    expect(component.onOptionClick.emit).toHaveBeenCalledWith(component.option)
+  })
+  it('should not emit event when clicked and disabled', () => {
+    component.option = 'Option value'
+    component.isDisabled = true;
+    spyOn(component.onOptionClick, 'emit')
+    let li = fixture.debugElement.query(By.css('li'));
+    li.nativeElement.click();
+    fixture.detectChanges();
+    expect(component.onOptionClick.emit).not.toHaveBeenCalled();
+  })
+
+  it('should emit event when keydown', () => {
+    component.option = 'Option value'
+    component.isDisabled = false;
+    spyOn(component.onOptionKeydown, 'emit')
+    let li = fixture.debugElement.query(By.css('li'));
+    (li.nativeElement as HTMLLIElement).dispatchEvent(new KeyboardEvent("keydown", { key: 'ArrowDown' }));
+    fixture.detectChanges();
+    expect(component.onOptionKeydown.emit).toHaveBeenCalled()
+  })
 });
--- projects/manng-components/src/lib/multiselect/multiselect.component.spec.ts
+++ projects/manng-components/src/lib/multiselect/multiselect.component.spec.ts
@@ -1,7 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { MultiselectComponent } from './multiselect.component';
 import { MultiselectItemComponent } from './multiselect-item/multiselect-item.component';
+import { By } from '@angular/platform-browser';
 
 describe('MultiselectComponent', () => {
   let component: MultiselectComponent;
@@ -9,9 +10,9 @@ describe('MultiselectComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MultiselectComponent, MultiselectItemComponent ]
+      declarations: [MultiselectComponent, MultiselectItemComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -23,4 +24,58 @@ describe('MultiselectComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call search method', () => {
+    spyOn(component, 'search')
+    let input = fixture.debugElement.query(By.css('input[type=text]'));
+    let event = new Event('input', {
+      bubbles: true,
+      cancelable: true,
+    });
+    (input.nativeElement as HTMLInputElement).dispatchEvent(event);
+    fixture.detectChanges();
+    expect(component.search).toHaveBeenCalled()
+  })
+
+  it('should display correct search result with field value', fakeAsync(() => {
+    component.field = 'value';
+    component.options = [{
+      value: 'Option 1'
+    },
+    {
+      value: "Option 2"
+    }]
+    component.search({ target: { value: '2' } })
+    tick(50)
+    fixture.detectChanges();
+    expect(component.renderedOptions.length).toBe(1)
+  }))
+
+  it('should display correct search result without field value', fakeAsync(() => {
+    component.options = ['Option 1', 'Option 2']
+    component.search({ target: { value: 'on' } })
+    tick(50)
+    fixture.detectChanges();
+    expect(component.renderedOptions.length).toBe(2)
+  }))
+
+  it('should check if option is selected', () => {
+    component.options = ['Option 1', 'Option 2'];
+    component.valuesSelected = ['Option 1'];
+    fixture.detectChanges()
+    expect(component.isOptionSelected(component.options[0])).toBeTruthy()
+  })
+
+  it('should get selected option index', () => {
+    component.options = ['Option 1', 'Option 2'];
+    component.valuesSelected = ['Option 1'];
+    fixture.detectChanges()
+    expect(component.getSelectedOptionIndex(component.options[0])).toBe(0)
+  })
+  it('should uncheck all items', () => {
+    spyOn(component.onSelect, 'emit')
+    component.uncheckAll()
+    fixture.detectChanges()
+    expect(component.onSelect.emit).toHaveBeenCalledWith([])
+  })
 });
